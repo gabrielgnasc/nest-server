db28c552441638532237e0de7e0e2d6caaf5282f
Gabriel Nascimento <ggnascimento.98@gmail.com>
2022-06-16 01:03:50 -0300

    fix: fix class-validator

--- src/common/dtos/user/update-password.dto.ts
+++ src/common/dtos/user/update-password.dto.ts
@@ -10,7 +10,7 @@ export class UpdatePasswordDTO {
 
   @ApiProperty({ required: true })
   @IsNotEmpty({ message: 'The password cannot be empty.' })
-  @IsEmail({ message: 'The password is not valid' })
+  @IsString({ message: 'The password must to be a string' })
   @Length(6, 20, { message: 'The password must be 6 to 20 characters' })
   newPassword: string;
 }
--- src/main.ts
+++ src/main.ts
@@ -1,4 +1,4 @@
-import { Logger } from '@nestjs/common';
+import { Logger, ValidationPipe } from '@nestjs/common';
 import { ConfigService } from '@nestjs/config';
 import { NestFactory } from '@nestjs/core';
 import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
@@ -8,6 +8,8 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   const configService = app.get(ConfigService);
+
+  app.useGlobalPipes(new ValidationPipe({ whitelist: true, transform: true }));
 
   const options = new DocumentBuilder()
     .setTitle("Base App API's")
